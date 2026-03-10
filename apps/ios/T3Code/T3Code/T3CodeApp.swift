42c71e3d2f0710ebe8510a426681bb1fac289fd7
Jakob Langtry <113549052+jjl4287@users.noreply.github.com>
2026-03-10 05:25:48 -0400

    ios version

---
+++ apps/ios/T3Code/T3Code/T3CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  T3CodeApp.swift
+//  T3Code
+//
+//  Created by Jakob Langtry on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct T3CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/ios/T3Code/T3CodeTests/T3CodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  T3CodeTests.swift
+//  T3CodeTests
+//
+//  Created by Jakob Langtry on 3/10/26.
+//
+
+import Testing
+@testable import T3Code
+
+struct T3CodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
